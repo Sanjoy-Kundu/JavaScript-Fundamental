431a66dee441293293d02bd9b23a329b5500fbf2
sanjoy <sanjoykundu187@gamil.com>
2023-01-20 23:48:41 +0600

    js fundamental

---
+++ assets/js/valiable.js
@@ -0,0 +1,21 @@
+/*
+*javascript variable 3 types (numeric, boolean, string)
+*/
+
+//numeric variable
+var age = 30;
+var price = 10.5;
+console.log(age);
+console.log(price);
+
+//string type 
+var person_name = "Anamul Haque Rubel";
+var address = "Nilfamari, Bangladesh";
+console.log(person_name);
+console.log(address);
+
+//Boolean 
+var rubelSerious = true;
+var isPassed = false;
+console.log(isPassed);
+console.log(rubelSerious);

42898435aea2f0775215fe959714e14b3dafb643
sanjoy <sanjoykundu187@gamil.com>
2023-01-21 00:32:08 +0600

    name concation added

---
+++ assets/js/js string concation.js
@@ -0,0 +1,14 @@
+console.log("JavaScript string Concatination , pareInt, pareFloat  Algorithom");
+
+//name without concatination
+var firstName = "Liton";
+var secondName = "Das";
+var fullName = firstName + secondName;
+console.log(fullName);
+
+
+//name withConcatinaton
+var name_one = "Bijoy";
+var name_two = "Kumar";
+var finalName = name_one + " " + name_two;
+console.log(finalName);

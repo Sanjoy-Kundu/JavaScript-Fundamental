9e2cd1ccde24c82d584d1fd8df929195e075f45b
sanjoy <sanjoykundu187@gamil.com>
2023-01-21 00:17:23 +0600

    rice pirice add 10 takak

--- assets/js/variable_value_update.js
+++ assets/js/variable_value_update.js
@@ -4,3 +4,14 @@ console.log("JavaScript Variable Value update");
 var potatoPrice = 35;
 potatoPrice = 100;
 console.log(potatoPrice);
+
+
+//js previous variable value change
+var eggPrice = 40;
+var ricePrice = 75;
+ricePrice = 55;
+console.log(ricePrice); // 55
+
+//rice price add 10 taka
+ricePrice = ricePrice + 10;
+console.log(ricePrice); //65

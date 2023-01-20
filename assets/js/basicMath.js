4935a957fe6b89281d20df6d1235840643661fcf
sanjoy <sanjoykundu187@gamil.com>
2023-01-21 00:01:04 +0600

    js basic math add

---
+++ assets/js/basicMath.js
@@ -0,0 +1,26 @@
+console.log('Basic Math for Javascript');
+/*
+::::::::::::::::Basic Math::::::::
+*Before learn js you can learn what is number 
+1. number ====> 12, 10, 11, 20
+2. Integer  =====> 100, 111, 222, 333
+3. Float    ======> 10.11, 22.33, 10.01
+4. Operation ====> add(+), multi(*), sub(-), division(/), modulus(%)
+5. Percent ======> %
+6. Average Marks ===> 
+7. Comparison Operator ===> <, > !=, ==, <=, >=
+8. Oiki Math ===> 1 apple = 10tk, 10 apple is = 10 * 10 = 100
+9. max and minimum number = 10, 12, 20, 33, 40, 55, 500 
+	find max number = 500 and min number = 10 
+10. series = 1 to 100, even number + odd number 
+
+*/
+
+
+//JavaScript Addition
+var fishPrice = 100;
+var vegetablePrice = 55;
+var eggPrice = 10;
+var netPrice = fishPrice + vegetablePrice + eggPrice;
+console.log(netPrice);
+
